f5fe085a9e2fce4c92c9e33a72613192ff1396a4
weber <weber50432@gmail.com>
2025-03-03 22:26:13 +0000

    adding Llama 3.2 3B with learning rate = 1e-6

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 # Install unbuffer if needed: brew install expect
 fine_tune_type="lora"
-model="meta-llama/Meta-Llama-3.1-8B-Instruct"  # Add this line
-job_name="${fine_tune_type}-Meta-Llama-3.1-8B-Instruct-lr-5"
+model="meta-llama/Llama-3.2-3B-Instruct"  # Add this line
+job_name="${fine_tune_type}-Llama-3.2-3B-Instruct-lr-6"
 data_path=../data/poker-total
 output_path_base=../adapters/${job_name}
 # adapter_file=../adapters/lora-Meta-Llama-3-8B-Instruct-lr-5/adapters.safetensors
@@ -22,12 +22,12 @@ python -m mlx_lm.lora \
     --data $data_path \
     --iters 5000 \
     --num-layers 16 \
-    --batch-size 2 \
+    --batch-size 4 \
     --val-batches 25 \
     --test-batches 100 \
     --steps-per-report 100 \
     --steps-per-eval 100 \
-    --learning-rate 0.00001 \
+    --learning-rate 0.000001 \
     2>&1 | tee -a "$log_file"
     # --resume-adapter-file $adapter_file\
     # --config "./config.yaml" \
